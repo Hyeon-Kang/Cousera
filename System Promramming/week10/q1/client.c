3d371063e420a1fbebd7ba1655bcaf4a020a6c9f
Hyeon <rkd099@naver.com>
2019-11-04 20:33:24 +0900

    Create client.c

---
+++ System Promramming/week10/q1/client.c
@@ -0,0 +1,11 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#define PORTNUM 9001 // 서버측 포트 번호
